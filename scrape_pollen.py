ee563122f732bfdef5ed877afe0f09975b4afb8a
Mervyn Galvin <mervyn511@gmail.com>
2021-04-28 13:18:35 +0100

    fix PEP8 issues, use a list for dates, tidy code

--- scrape_pollen.py
+++ scrape_pollen.py
@@ -4,12 +4,15 @@ import requests
 from bs4 import BeautifulSoup
 from datetime import datetime
 
+
 class PollenResult(dict):
     def __init__(self, region, date_for, measurement):
-      dict.__init__(self, region = region, date_for = date_for, measurement = measurement)
+        dict.__init__(self, region=region, date_for=date_for,
+                      measurement=measurement)
 
 
-standard_date = '%Y-%d-%m' # ISO 8601
+standard_date = '%Y-%d-%m'  # ISO 8601
+met_date = '%d-%m-%Y'
 URL = "https://www.met.ie/forecasts/pollen"
 page = requests.get(URL)
 
@@ -22,11 +25,10 @@ rows = result_table.findChildren('tr')
 
 date_row = rows[1].findChildren('th')
 
-first_date = datetime.strptime(date_row[1].string, '%d-%m-%Y')
-second_date = datetime.strptime(date_row[2].string, '%d-%m-%Y')
-third_date = datetime.strptime(date_row[3].string, '%d-%m-%Y')
+date_res = []
+for i in range(1, 4):
+    date_res.append(datetime.strptime(date_row[i].string, met_date))
 
-# TODO: The dates should be put in a list to be refferred to later on.
 
 region_rows = rows[2:]
 pollen_results = []
@@ -34,8 +36,7 @@ pollen_results = []
 for region_row in region_rows:
     data_fields = region_row.findChildren('td')
     region = data_fields[0].string
-    # TODO: When the above TODO is fixed, this should be changed to iterate over each data_field[1-3], dates[0-2]
-    pollen_results.append(PollenResult(region, first_date.strftime(standard_date), data_fields[1].string))
-    pollen_results.append(PollenResult(region, second_date.strftime(standard_date), data_fields[2].string))
-    pollen_results.append(PollenResult(region, third_date.strftime(standard_date), data_fields[3].string))
-
+    for i in range(1, 4):
+        pollen_results.append(PollenResult(region,
+                              date_res[i-1].strftime(standard_date),
+                              data_fields[i].string))
